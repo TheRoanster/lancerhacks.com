ca50e82670acb00cc10d801b46254044e9858f0d
Nitish Gourishetty <nitishgourishetty@gmail.com>
2023-01-21 12:57:57 -0800

    temp site good

--- components/sponsors/Sponsors.tsx
+++ components/sponsors/Sponsors.tsx
@@ -5,11 +5,12 @@ export function Sponsors() {
   let sponsors = [
     ['wolfram', 'https://www.wolfram.com'],
     ['balsamiq','https://balsamiq.com/wireframes/'],
-    ['slingshot', 'https://slingshotahead.com/'],
+    ['slingshot2', 'https://slingshotahead.com/'],
     ['jetbrains', 'jb.gg/studentlicense'],
     ['pizzafund', 'https://organize.mlh.io/host/pizza-fund-event'],
     ['radix', 'https://radix.website/'],
-    ['github', 'https://github.com/']
+    ['github', 'https://github.com/'],
+    ['cookiedb', 'https://cookiedb.com/']
 ]
 
   return (
@@ -18,8 +19,8 @@ export function Sponsors() {
       {
         sponsors.map((sponsor)=> <a key={sponsor[0]} href={sponsor[1]}><img className={styles.sponsor} alt={sponsor[0]} src={`/sponsors/${sponsor[0]}.png`}/></a>)
       }
-        <h2 >To sponsor our event, email us at hello@lancerhacks.com. Feel free to read our sponsorship prospectus below.</h2>
-        <a className={styles.sponsor_prospectus} href="https://docs.google.com/document/d/1WjlYQLC2-Ryv-3X7zEn0kZ4c-ZbXSRQcxSvl-Ap9RZM/edit?usp=sharing">Sponsorship Prospectus</a>
+        {/*<h2 >To sponsor our event, email us at hello@lancerhacks.com. Feel free to read our sponsorship prospectus below.</h2>*/}
+        {/*<a className={styles.sponsor_prospectus} href="https://docs.google.com/document/d/1WjlYQLC2-Ryv-3X7zEn0kZ4c-ZbXSRQcxSvl-Ap9RZM/edit?usp=sharing">Sponsorship Prospectus</a>*/}
 
     </section>
   )
